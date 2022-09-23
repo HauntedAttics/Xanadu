b1f3fc3a6d83af81718288857b30dc447a63d933
HauntedAttics <925930071@qq.com>
2022-09-24 00:25:41 +0800

    wallpaper update

--- src/pages/wallpapers/index.js
+++ src/pages/wallpapers/index.js
@@ -34,37 +34,37 @@ export default memo(function Wallpapers() {
       <Divider/>
       <div className='grid'>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=25' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=26' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=27' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=26' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=28' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=29' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom30' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=31' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/420?radom=1' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/400?radom=2' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=1' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=2' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=3' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=4' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=5' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=6' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=7' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/420?radom=8' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=8' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=9' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=10' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=11' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=12' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/420?radom=13' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=13' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=14' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=15' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=16' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=17' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/420?radom=18' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=18' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=19' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=20' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=21' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=22' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=23' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=24' /></div>   
-        <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=32' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=32' /></div>
       </div>
       <Divider/>
     </WallpapersWrapper>
