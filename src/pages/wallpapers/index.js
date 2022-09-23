4fd49539f2205a2683dfaee852ab4c0c34ab642e
HauntedAttics <925930071@qq.com>
2022-09-24 01:05:21 +0800

    wallpaper update

--- src/pages/wallpapers/index.js
+++ src/pages/wallpapers/index.js
@@ -33,23 +33,27 @@ export default memo(function Wallpapers() {
       <h2>Here are some wallpapers randomly obtained from Unsplash. Enjoy!</h2>
       <Divider/>
       <div className='grid'>
-        <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=25' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/540?radom=27' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/440?radom=26' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=28' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/470?radom=25' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=27' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/450?radom=26' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/490?radom=28' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/540?radom=29' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/520?radom30' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=31' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=1' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=2' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=3' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=32' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/510?radom=33' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=34' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/530?radom=35' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/510?radom30' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/470?radom=31' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/530?radom=1' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=2' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/510?radom=3' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=4' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=5' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=6' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=7' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=8' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=9' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/540?radom=10' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/530?radom=10' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=11' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=12' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=13' /></div>
@@ -62,9 +66,9 @@ export default memo(function Wallpapers() {
         <div className='grid-item'><Image src='https://picsum.photos/360/540?radom=20' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=21' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/520?radom=22' /></div>
-        <div className='grid-item'><Image src='https://picsum.photos/360/500?radom=23' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/510?radom=23' /></div>
         <div className='grid-item'><Image src='https://picsum.photos/360/480?radom=24' /></div>   
-        <div className='grid-item'><Image src='https://picsum.photos/360/460?radom=32' /></div>
+        <div className='grid-item'><Image src='https://picsum.photos/360/470?radom=32' /></div>
       </div>
       <Divider/>
     </WallpapersWrapper>
